083bfe8117f9ec1a1631448a409c8cbc31447bf8
Rendy I <risdwanta35@gmail.com>
2022-12-12 21:15:18 +0700

    slight optimization

--- sorting.c
+++ sorting.c
@@ -32,21 +32,18 @@ void swap_element(int *i, int *j)
 
 int *bubble_sort(int *arr, int n)
 {
-    int *sorted = copy_arr(arr, 10);
-
     printf("%s: ", __func__);
 
     for(int i = 0; i<n-1; i++){
         for(int j = 0; j<n-i-1; j++){
-            if(sorted[j] > sorted[j+1]) swap_element(&sorted[j], &sorted[j+1]);
+            if(arr[j] > arr[j+1]) swap_element(&arr[j], &arr[j+1]);
         }
     }
-    return sorted;
+    return arr;
 }
 
 int *selection_sort(int *arr, int n)
 {
-    int *sorted = copy_arr(arr, 10);
     int min_idx;
 
     printf("%s: ", __func__);
@@ -55,19 +52,19 @@ int *selection_sort(int *arr, int n)
         min_idx = i;
 
         for(int j = i+1; j<n; j++){
-            if(sorted[j] < sorted[min_idx]) min_idx = j;
+            if(arr[j] < arr[min_idx]) min_idx = j;
         }
 
-        swap_element(&sorted[min_idx], &sorted[i]);
+        swap_element(&arr[min_idx], &arr[i]);
     }
-    return sorted;
+    return arr;
 }
 
 // -----------------------------------------------------------------------------
 
 void check_algo(int* (*sort)(int*, int), int *to_be_sorted, int *ground_truth, int n)
 {
-    int *sorted_arr = (*sort)(to_be_sorted, n);
+    int *sorted_arr = (*sort)(copy_arr(to_be_sorted, n), n);
 
     for(int i = 0; i < n; i++){
         if(sorted_arr[i] != ground_truth[i]){
