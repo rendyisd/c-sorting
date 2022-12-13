361e9ac8807f9639da22d1c19238853458c830de
Rendy I <risdwanta35@gmail.com>
2022-12-13 10:21:38 +0700

    insertion and quick

--- sorting.c
+++ sorting.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ARR_SIZE 20
+
 int *copy_arr(int *arr, int n)
 {
-    int *ret_arr = (int*)malloc(n * sizeof(int));
+    int *ret_arr = (int *)malloc(n * sizeof(int));
 
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i < n; i++){
         ret_arr[i] = arr[i];
     }
     return ret_arr;
@@ -13,7 +15,7 @@ int *copy_arr(int *arr, int n)
 
 void print_arr(int *arr, int n)
 {
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i < n; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
@@ -34,7 +36,7 @@ int *bubble_sort(int *arr, int n)
 {
     printf("%s: ", __func__);
 
-    for(int i = 0; i<n-1; i++){
+    for(int i = 0; i < n-1; i++){
         for(int j = 0; j<n-i-1; j++){
             if(arr[j] > arr[j+1]) swap_element(&arr[j], &arr[j+1]);
         }
@@ -48,15 +50,77 @@ int *selection_sort(int *arr, int n)
 
     printf("%s: ", __func__);
 
-    for(int i = 0; i<n-1; i++){
+    for(int i = 0; i < n-1; i++){
         min_idx = i;
 
-        for(int j = i+1; j<n; j++){
+        for(int j = i+1; j < n; j++){
             if(arr[j] < arr[min_idx]) min_idx = j;
         }
 
         swap_element(&arr[min_idx], &arr[i]);
     }
+    return arr;
+}
+
+int *insertion_sort(int *arr, int n)
+{
+    printf("%s: ", __func__);
+
+    for(int i = 0; i < n-1; i++){
+        if(arr[i] > arr[i+1]){
+            swap_element(&arr[i], &arr[i+1]);
+            for(int j = i; j > 0; j--){
+                if(arr[j] < arr[j-1]) swap_element(&arr[j], &arr[j-1]);
+            }
+        }
+    }
+
+    return arr;
+}
+
+// Hope this has no memory leak
+int *quick_sort(int *arr, int n)
+{
+    if(n <= 1) return arr;
+
+    int tmp_iter = 0;
+
+    int pivot = arr[0];
+
+    int *high = (int *)malloc((n-1) * sizeof(int));
+    int *low = (int *)malloc((n-1) * sizeof(int));
+
+    int high_iter = 0;
+    int low_iter = 0;
+
+    for(int i = 1; i < n; i++){
+        if(arr[i] <= pivot){
+            low[low_iter] = arr[i];
+            low_iter++;
+        }
+        else{
+            high[high_iter] = arr[i];
+            high_iter++;
+        }
+    }
+
+    high = quick_sort(high, high_iter);
+    low = quick_sort(low, low_iter);
+
+    for(int i = 0; i < low_iter; i++){
+        arr[tmp_iter] = low[i];
+        tmp_iter++;
+    }
+    arr[tmp_iter] = pivot;
+    tmp_iter++;
+    for(int i = 0; i < high_iter; i++){
+        arr[tmp_iter] = high[i];
+        tmp_iter++;
+    }
+
+    free(high);
+    free(low);
+
     return arr;
 }
 
@@ -69,6 +133,7 @@ void check_algo(int* (*sort)(int*, int), int *to_be_sorted, int *ground_truth, i
     for(int i = 0; i < n; i++){
         if(sorted_arr[i] != ground_truth[i]){
             printf("No Good\n");
+            free(sorted_arr);
             return;
         }
     }
@@ -78,14 +143,30 @@ void check_algo(int* (*sort)(int*, int), int *to_be_sorted, int *ground_truth, i
 
 int main()
 {
-    int init_arr[10] = {7, 3, 9, 2, 4, 1, 6, 10, 8, 5};
-    int sorted_arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9 , 10};
+    // Repeating numb
+    int init_arr[ARR_SIZE] = {
+        26, 28, 3, 57, 34, 8, 3, 12, 15, 85,
+        99, 42, 82, 15, 94, 15, 62, 49, 28, 29
+    };
+    int sorted_arr[ARR_SIZE] = {
+        3, 3, 8, 12, 15, 15, 15, 26, 28, 28,
+        29, 34, 42, 49, 57, 62, 82, 85, 94, 99
+    };
 
-    check_algo(bubble_sort, init_arr, sorted_arr, 10);
-    print_arr(init_arr, 10); // To check if we accidentally changed init_arr or not
+    // int init_arr[ARR_SIZE] = {7, 3, 9, 2, 4, 1, 6, 10, 8, 5};
+    // int sorted_arr[ARR_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9 , 10};
 
-    check_algo(selection_sort, init_arr, sorted_arr, 10);
-    print_arr(init_arr, 10);
+    // int init_arr[ARR_SIZE] = {12, 11, 13, 5, 6};
+    // int sorted_arr[ARR_SIZE] = {5, 6, 11, 12, 13};
+
+    check_algo(bubble_sort, init_arr, sorted_arr, ARR_SIZE);
+
+    check_algo(selection_sort, init_arr, sorted_arr, ARR_SIZE);
+
+    check_algo(insertion_sort, init_arr, sorted_arr, ARR_SIZE);
+
+    printf("quick_sort: ");
+    check_algo(quick_sort, init_arr, sorted_arr, ARR_SIZE);
 
     return 0;
 }
